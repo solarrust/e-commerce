995257eaee23379a79cd481d0720d13f5c0f6ed7
Alena Batitskaia <batickaya.a@gmail.com>
2025-02-03 14:53:09 +0000

    ThemeProvider added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,23 @@
 import type { Metadata } from "next";
+import { Roboto } from "next/font/google";
 
-import Footer from "../components/Footer/Footer";
-import Header from "../components/Header/Header";
+import Footer from "@/components/Footer/Footer";
+import Header from "@/components/Header/Header";
+import Provider from "@/components/Provider";
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider } from "@mui/material/styles";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
+
+import theme from "../theme";
 
 import "./globals.css";
+
+const roboto = Roboto({
+  weight: ["300", "400", "500", "700"],
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-roboto",
+});
 
 export const metadata: Metadata = {
   title: "E-commerce",
@@ -17,12 +31,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
-        <div className="page">
-          <Header />
-          {children}
-          <Footer />
-        </div>
+      <body className={roboto.variable}>
+        <AppRouterCacheProvider options={{ key: "css" }}>
+          <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <Provider>
+              <div className="page">
+                <Header />
+                {children}
+                <Footer />
+              </div>
+            </Provider>
+          </ThemeProvider>
+        </AppRouterCacheProvider>
       </body>
     </html>
   );
---
+++ src/theme.ts
@@ -0,0 +1,14 @@
+"use client";
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  cssVariables: true,
+  typography: {
+    fontFamily: "var(--font-roboto)",
+  },
+  palette: {
+    mode: "dark",
+  },
+});
+
+export default theme;
